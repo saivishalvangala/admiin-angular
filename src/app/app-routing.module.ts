c9c4b34c990b086e36291a22588d1c387254c5b3
saivishalvangala <saivishalvangala@gmail.com>
2019-11-18 16:30:33 +0530

    :tada: admin mainpage completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AdminComponent} from './admin/admin.component'
+import { CreatedPageComponent } from './created-page/created-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'createTest', component:AdminComponent},
+  {path:'testCreated/:id',component:CreatedPageComponent},
+  {path:' ', redirectTo:"/createTest", pathMatch:'full'},
+  {path:'**',component:AdminComponent  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
